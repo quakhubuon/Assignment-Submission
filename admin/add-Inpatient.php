c5e1b8a6f75bf6c45749d6c6bebb9e4bc93678f3
quakhubuon <tranminh2014.tm@gmail.com>
2022-11-27 06:26:36 +0700

    first commit

--- admin/add-Inpatient.php
+++ admin/add-Inpatient.php
@@ -196,7 +196,7 @@
             <div class="content">
                 <div class="row">
                     <div class="col-lg-8 offset-lg-2">
-                        <h4 class="page-title">Add Patient</h4>
+                        <h4 class="page-title">Add Inpatient</h4>
                     </div>
                 </div>
                 <div class="row">
